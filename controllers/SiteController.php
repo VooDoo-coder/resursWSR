992cb9e0c5cae34ac6e4691bfda7fe0bce9009b5
VooDoo-coder <savratskii@gmail.com>
2021-06-09 21:45:43 +0300

    0.4

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,4 +125,9 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+    public function actionTests()
+    {
+        return $this->render('tests');
+    }
 }
